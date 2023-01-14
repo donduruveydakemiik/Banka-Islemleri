55ffcc82846962c7ca2188e400c0d4bee720afbb
RÜVEYDA KEMİK <77358367+donduruveydakemiik@users.noreply.github.com>
2023-01-14 17:04:26 +0300

    Add files via upload

---
+++ algoritmatasarimi/Banka/AktifHesap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoritmaTasarimi.Banka
+{
+    public class AktifHesap : ITransfer
+    {
+        private decimal bakiye;
+        public decimal Bakiye => bakiye;
+
+        public bool Cek(decimal miktar)
+        {
+            if (bakiye >= miktar)
+            {
+
+                bakiye -= miktar;
+                return true;
+            }
+
+            Console.WriteLine("Akrif hesabınızda paranız yetersizdir.");
+            return false;
+        }
+
+        public bool transferYap(IBankaHesap aliciHesap, decimal miktar)
+        {
+            bool sonuc = Cek(miktar);
+            if (sonuc==true)
+            {
+                aliciHesap.Yatir(miktar);
+            }
+            return sonuc;
+        }
+
+        public void Yatir(decimal miktar)
+        {
+            bakiye += miktar;
+        }
+
+        public override string? ToString() =>
+            $"aktif banka bakiye bilginiz: {bakiye}";
+        
+    }
+}
---
+++ algoritmatasarimi/Banka/yatirimHesabi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoritmaTasarimi.Banka
+{
+    public class yatirimHesabi : IBankaHesap
+    {
+
+        private decimal bakiye; // field tanımı işlem yapılabilecek.
+        public decimal Bakiye => bakiye;// mevcut bakiye ne ise güncellemiş oluyoruz.
+
+        public bool Cek(decimal miktar)
+        {
+            if (bakiye >= miktar)
+            {
+                bakiye -= miktar;
+                return true;
+            }
+            Console.WriteLine("Yatırım hesabınızda bakiye yetersizdir yeniden deneyiniz.");
+            return false;
+        }
+
+        public void Yatir(decimal miktar) =>
+            bakiye += miktar;
+
+        public override string? ToString() =>
+            $" yatırım hesabı bakiye Bilgisi: {bakiye}";
+
+    }
+}
