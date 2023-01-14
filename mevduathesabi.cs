bd8a92fab9a52ab24cfd8436e7c181062ca5080f
RÜVEYDA KEMİK <77358367+donduruveydakemiik@users.noreply.github.com>
2023-01-14 16:44:54 +0300

    Add files via upload

---
+++ mevduathesabi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algoritmaTasarimi.Banka
+{
+    internal class mevduatHesabi : IBankaHesap
+    {
+        private decimal bakiye; // field tanımı işlem yapılabilecek.
+        public decimal Bakiye => bakiye;
+
+        public bool Cek(decimal miktar)
+        {
+           if (bakiye>=miktar)
+            {
+                bakiye -= miktar;
+                return true;
+            }
+            Console.WriteLine("\a Bakiye yetersizdir.");
+            return false;
+        }
+
+        public void Yatir(decimal miktar)=> bakiye += miktar;
+
+        public override string? ToString() =>
+            $"mevduat hesabı bakiye bilgisi: {bakiye}";        
+    }
+}
